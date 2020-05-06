8b918089adc8e813a4d228b9ea1c8963086a0fd0
Ohad Michel <ohad.michel@apeel.com>
2020-05-06 16:21:33 -0700

    Correct setup.py to include deployed models

--- {{ cookiecutter.repo_name }}/setup.py
+++ {{ cookiecutter.repo_name }}/setup.py
@@ -2,6 +2,7 @@ import setuptools
 import os
 import apeel_{{cookiecutter.repo_name}}
 
+
 def populate_dependencies(filename='requirements.txt'):
     """
     this function reads external packages from
@@ -18,7 +19,7 @@ def populate_dependencies(filename='requirements.txt'):
     try:
         with open(filename) as file:
             for line in file:
-                if  save_to_install_list and line.strip():
+                if save_to_install_list and line.strip():
                     requirements += [line.split('=')[0].rstrip()]
                 if string_to_match in line and save_to_install_list == False:
                     save_to_install_list = True
@@ -27,32 +28,12 @@ def populate_dependencies(filename='requirements.txt'):
 
     return requirements
 
-def get_model_files():
-    """
-    This function looks under models/deployed
-    directory to get the relative path of
-    *.joblib files.
-    :return: relative path of .joblib files
-    """
-    current_directory_path = os.path.dirname(os.path.realpath(__file__))
-    current_directory = os.path.basename(current_directory_path)
-    model_directory = os.path.join('apeel_' + current_directory, 'models/deployed')
-
-    model_paths = []
-    for (path, directories, filenames) in os.walk(model_directory):
-        for filename in filenames:
-            if ".joblib" in filename:
-                model_paths.append(os.path.join(path, filename))
-    return model_paths
 
 # Get the version of this package
 version = apeel_{{cookiecutter.repo_name}}.version
 
-#Populate external requirements from requirements.txt
+# Populate external requirements from requirements.txt
 external_reqs = populate_dependencies('requirements.txt')
-
-#Get model joblib files
-model_files = get_model_files()
 
 # Get the long description of this package
 with open("README.md", "r") as fh:
@@ -69,7 +50,7 @@ setuptools.setup(
     url="https://gitlab.com/apeelsciences/datascience/projects/{{cookiecutter.repo_name}}",
     packages=setuptools.find_packages(exclude=['unit_tests']),
     install_requires=['apeel_datatools'] + external_reqs,
-    #package_data={'apeel_{{cookiecutter.repo_name}}': model_files},
+    include_package_data=True,
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
